43e32c4b296023673816a7cfb52e664a4c9a0961
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-09-24 13:56:31 -0400

    Added partial form for editing recipes

--- Client/src/pages/Edit.jsx
+++ Client/src/pages/Edit.jsx
@@ -1,7 +1,28 @@
+const React = require('react');
 
 
-function Edit() {
-    
+function Edit({recipe}) {
+    return (
+        <Default>
+            <h2>Making Changes</h2>
+            <form>
+                <label for="name"></label>
+                <input type="text"
+                    name="name"
+                    id="name"
+                    required
+                    defaultValue={recipe.name} />
+                
+                <label for="image">Image</label>
+                <input type="tex"
+                    name="image"
+                    id="image"
+                    defaultValue={recipe.name} />
+                
+                <input type="submit"></input>
+            </form>
+        </Default>
+    )
 }
 
 export default Edit;
